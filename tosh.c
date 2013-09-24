f8d15093367e42c7ecdc42979ec034f8db9e6be4
enigmadan <enigmadan@gmail.com>
2013-09-24 10:43:47 -0700

    Update tosh.c

--- tosh.c
+++ tosh.c
@@ -53,12 +53,20 @@ printf( " argv[%d] %s\n", count, argv[count] );
     ball[0] = NULL;
 
     while(1) {
-        printf("%s[tosh %s]$%s", "\x1B[36m", folder, "\x1B[0m");
+        printf("%s[tosh %s]$%s ", "\x1B[36m", folder, "\x1B[0m");
         if((read = getline(&line, &len, stdin)) != -1){
+            int pipe = 0;
+            int iored = 0;
             line = strtok(line, "\n"); //remove the newline character
             yarn = strtok(line, " "); //tokenize by spaces
             int h = 0;
             while(yarn!=NULL && h<19){
+                if(strcmp(yarn,"|")==0){
+                    pipe = 1;
+                }
+                if(strcmp(yarn,"<")==0||strcmp(yarn,">")==0){
+                    iored = 1;
+                }
                 //printf("%d",h);
                 ball[h] = yarn;
                 //printf(">%s<\n", yarn); //printing current token
@@ -67,49 +75,57 @@ printf( " argv[%d] %s\n", count, argv[count] );
                 h++;
             }
             ball[h] = NULL;
-            if(h<20){
-                if(ball[0]==NULL){
-                    //printf("enter something please\n");
-                }else if(strcmp(ball[0],"exit")==0){
-                    return 0;
-                }else if(strcmp(ball[0],"cd")==0){
-                    if (chdir(ball[1]) != 0) //if chdir failed,
-                        perror(""); //print the error
-                    else //else chdir worked,
-                        ptr = getcwd(cwd,(size_t)pathconf(".", _PC_PATH_MAX));//get the new cwd
-                        strcpy(ptr_too,ptr);
-                    foldertmp = strtok(ptr_too, "/");
-                    while(foldertmp != NULL){
-                        folder = foldertmp;
-                        foldertmp = strtok(NULL, "/");
+            if(h<19){
+                if(pipe == 0 && iored == 0){
+                    if(ball[0]==NULL){
+                        //printf("enter something please\n");
+                    }else if(strcmp(ball[0],"exit")==0){
+                        return 0;
+                    }else if(strcmp(ball[0],"cd")==0){
+                        if (chdir(ball[1]) != 0) //if chdir failed,
+                            perror(""); //print the error
+                        else //else chdir worked,
+                            ptr = getcwd(cwd,(size_t)pathconf(".", _PC_PATH_MAX));//get the new cwd
+                            strcpy(ptr_too,ptr);
+                        foldertmp = strtok(ptr_too, "/");
+                        while(foldertmp != NULL){
+                            folder = foldertmp;
+                            foldertmp = strtok(NULL, "/");
+                        }
+                    }else{
+                        int cpid = fork();
+                        if(cpid == -1) //fork failed
+                            printf("fork failed. ...now what...?");
+                        else if (cpid == 0) {//this is the child process
+                            execvp(ball[0],ball);
+                            perror("");
+                            //printf("Unknown command.\n");
+                            exit(0);
+                            //printf("returning %d\n", cpid);
+                        }
+                        else { //this is the parent process
+                            //pid =
+                            waitpid(cpid, &status, 0);
+                        }
+                        //return cpid;
                     }
+                }else if(pipe == 1){
+                    printf("Pipe?\n");
+                }else if(iored == 1){
+                    printf("IO redirect\n");
                 }else{
-                    int cpid = fork();
-                    if(cpid == -1) //fork failed
-                        printf("fork failed. ...now what...?");
-                    else if (cpid == 0) {//this is the child process
-                        execvp(ball[0],ball);
-                        printf("Unknown command.\n");
-                        exit(0);
-                        //printf("returning %d\n", cpid);
-                    }
-                    else { //this is the parent process
-                        //pid = 
-                        waitpid(cpid, &status, 0);
-                    }
-                    printf("\n");
-                    //return cpid;
+
                 }
 
                 /*
-                printf("%d\n", h);
-                int i=0;
-                for(i=0;i<h;i++){
-                    printf("%s\n", ball[i]);
-                    printf("%d\n", i);
-                }//*/
+printf("%d\n", h);
+int i=0;
+for(i=0;i<h;i++){
+printf("%s\n", ball[i]);
+printf("%d\n", i);
+}//*/
             }else{
-                printf("Too many arguments.\n");
+                printf("Too many arguments\n");
             }
 
         }
@@ -124,59 +140,59 @@ printf( " argv[%d] %s\n", count, argv[count] );
 
 
       /*
-                //exit the terminal
-                if(strcmp(yarn,"exit")==0){
-                    return 0;
-                }
+//exit the terminal
+if(strcmp(yarn,"exit")==0){
+return 0;
+}
 
-                //change the working directory
-                else if(strcmp(yarn,"cd")==0){
-                    yarn = strtok(NULL, " ");//move to next token
-                    if (chdir(yarn) != 0) //if chdir failed,
-                        perror(""); //print the error
-                    else //else chdir worked,
-                        ptr = getcwd(cwd,(size_t)pathconf(".", _PC_PATH_MAX));//get the new cwd
-                        strcpy(ptr_too,ptr);
-                    foldertmp = strtok(ptr_too, "/");
-                    while(foldertmp != NULL){
-                        folder = foldertmp;
-                        foldertmp = strtok(NULL, "/");
-                    }
-                    break;
-                }
+//change the working directory
+else if(strcmp(yarn,"cd")==0){
+yarn = strtok(NULL, " ");//move to next token
+if (chdir(yarn) != 0) //if chdir failed,
+perror(""); //print the error
+else //else chdir worked,
+ptr = getcwd(cwd,(size_t)pathconf(".", _PC_PATH_MAX));//get the new cwd
+strcpy(ptr_too,ptr);
+foldertmp = strtok(ptr_too, "/");
+while(foldertmp != NULL){
+folder = foldertmp;
+foldertmp = strtok(NULL, "/");
+}
+break;
+}
 
-                //print the working directory
-                else if(strcmp(yarn,"pwd")==0){
-                    int cpid = fork();
-                    if(cpid == -1) //fork failed
-                        printf("fork failed. ...now what...?");
-                    else if (cpid == 0) {//this is the child process
-                        printf("%s\n", ptr);
-                        //printf("returning %d\n", cpid);
-                        return cpid;
-                    }
-                    else { //this is the parent process
-                        //pid = 
-                        waitpid(cpid, &status, 0);
+//print the working directory
+else if(strcmp(yarn,"pwd")==0){
+int cpid = fork();
+if(cpid == -1) //fork failed
+printf("fork failed. ...now what...?");
+else if (cpid == 0) {//this is the child process
+printf("%s\n", ptr);
+//printf("returning %d\n", cpid);
+return cpid;
+}
+else { //this is the parent process
+//pid =
+waitpid(cpid, &status, 0);
 
-                        //printf("cpid=%d\n",cpid);
-                        //printf("just recieved %d\n", pid);
-                    }
-                    break;
-                }
+//printf("cpid=%d\n",cpid);
+//printf("just recieved %d\n", pid);
+}
+break;
+}
 
-                //list files in current directory
-                else if(strcmp(yarn,"ls")==0){
-                    //ls();
-                    break;
-                }
-                else if(strcmp(yarn,"cat")){
-                    //cat(argc,argv);
-                    break;
-                }
+//list files in current directory
+else if(strcmp(yarn,"ls")==0){
+//ls();
+break;
+}
+else if(strcmp(yarn,"cat")){
+//cat(argc,argv);
+break;
+}
 
-                //else command not found
-                else {
-                    printf("%s: Command not found.\n", yarn);
-                    break;
-                }*/
+//else command not found
+else {
+printf("%s: Command not found.\n", yarn);
+break;
+}*/
